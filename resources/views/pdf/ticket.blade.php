b8c870ee0b89b3328cbf3b78df8f81391a27db97
vicSystems <victorasuquob@gmail.com>
2022-09-25 17:04:48 +0100

    referral notifier

--- resources/views/pdf/ticket.blade.php
+++ resources/views/pdf/ticket.blade.php
@@ -1,3 +1,3 @@
-<img src="{{asset('images/reliance_logo.png')}}" alt="Logo" height="75px">
+<img src="{{asset('images/reliance_logo2.jpeg')}}" alt="Logo" height="75px">
 
 {{asset('images/reliance_logo.png')}}
